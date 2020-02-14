c5ecb065e96b2c9ddcc3b1ff1dbeb01ed97dc354
Serg <sb@idaproject.com>
2020-02-14 17:03:49 +0300

    update eslint, move mixins to assets

--- assets/js/directives/clickoutside.js
+++ assets/js/directives/clickoutside.js
@@ -21,9 +21,9 @@ function createDocumentHandler(el, binding, vnode) {
             el.contains(mouseup.target) ||
             el.contains(mousedown.target) ||
             el === mouseup.target ||
-            (vnode.context.popperElm &&
-                (vnode.context.popperElm.contains(mouseup.target) ||
-                    vnode.context.popperElm.contains(mousedown.target)))) return;
+            (vnode.context.popperEl &&
+                (vnode.context.popperEl.contains(mouseup.target) ||
+                    vnode.context.popperEl.contains(mousedown.target)))) return;
 
         if (binding.expression &&
             el[ctx].methodName &&
